36c047458d76c0ef91c306cf1d3e4197ce1960d1
Freddie Sze <freddie@y714.com>
2019-07-02 01:20:05 +0800

    change site title, sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,18 @@
+const toc = require('./toc.js');
+
 module.exports = {
   dest: 'dist',
-  title: 'Sutta·Sutta',
+  title: 'SUTTA·SUTTA',
   description: 'Compare sutta translations',
+  themeConfig: {
+    nav: [
+      { text: 'DN', link: '/dn/' },
+      { text: 'MN', link: '/mn/' },
+      { text: 'SN', link: '/sn/' },
+      { text: 'AN', link: '/an/' },
+    ],
+    sidebar: toc,
+    sidebarDepth: 2,
+    displayAllHeaders: true
+  }
 }
