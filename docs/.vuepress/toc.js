46e7ea801a2fb0c731db5b1c27d0ea357cf24fe4
Freddie Sze <freddie@y714.com>
2020-02-26 22:04:33 +0800

    fixed sidebar links

--- docs/.vuepress/toc.js
+++ docs/.vuepress/toc.js
@@ -1,10 +1,10 @@
 const toc = [
-  ['/dn', 'DN'],
-  ['/dn1', 'DN1'],
-  ['/dn2', 'DN2'],
-  ['/an', 'AN'],
-  ['/an1', 'AN1'],
-  ['/an2', 'AN2']
+  ['/dn/', 'DN'],
+  ['/dn1/', 'DN1'],
+  ['/dn2/', 'DN2'],
+  ['/an/', 'AN'],
+  ['/an1/', 'AN1'],
+  ['/an2/', 'AN2']
 ]
 
 module.exports = toc;
